e29750ea1b42c78487949133d2681d4f182c0aa5
Tristanchaang <tristan.chaang@gmail.com>
2024-07-21 06:15:15 -0400

    mouse follower

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import { select } from 'd3'
+const svg = d3.select('#app').append('svg');
 
-const svg = select('#app').append('svg')
+const circle = svg.append('circle');
+
+svg.attr('width', window.innerWidth)
+svg.attr('height', window.innerHeight)
+
+circle.attr('r', 30);
+
+svg.on('mousemove', (event) => {
+    console.log(event)
+    circle.attr('cx', event.x);
+    circle.attr('cy', event.y);
+});
