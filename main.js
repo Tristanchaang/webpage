513e24db1af64b09cc309ab0aef83e325d489eb7
Tristanchaang <tristan.chaang@gmail.com>
2024-07-21 07:04:59 -0400

    zeroed overflow

--- main.js
+++ main.js
@@ -5,13 +5,10 @@ const circle = svg.append('circle');
 svg.attr('width', window.innerWidth)
 svg.attr('height', window.innerHeight)
 
-circle.attr('cx', 50)
-circle.attr('cy', 50)
-
 circle.attr('r', 30);
 
 svg.on('mousemove', (event) => {
-    console.log(event)
     circle.attr('cx', event.x);
     circle.attr('cy', event.y);
 });
+
