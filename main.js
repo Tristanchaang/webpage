d32ab45b9c45f6b70538d9b35669fbc26623bae7
Tristanchaang <tristan.chaang@gmail.com>
2024-07-22 01:12:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const svg = d3.select('#app').append('svg');
 const hallo4 = svg.append('rect');
-const hallo5 = svg.append('rect');
 const hi = svg.append('polygon')
 
 hi.attr('points', '100,100 150,100 150,200 250,200 250,350 200,350 200,250 150,250 150,350 100,350')
@@ -18,10 +17,6 @@ hallo4.attr('y', 200);
 hallo4.attr('height', 150);
 hallo4.attr('width', 50);
 
-hallo5.attr('x', 300);
-hallo5.attr('y', 100);
-hallo5.attr('height', 50);
-hallo5.attr('width', 50);
 
 svg.on('mousemove', (event) => {
     circle.attr('cx', event.x);
