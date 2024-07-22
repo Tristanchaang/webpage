af6f12728caebd15ca3d16f6a098ebf0af5e6474
Tristanchaang <tristan.chaang@gmail.com>
2024-07-22 12:31:57 -0400

    fade

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const svg = d3.select('#app').append('svg');
-const hallo4 = svg.append('rect');
 const hi = svg.append('polygon')
+const hi2 = svg.append('polygon')
+const hi3 = svg.append('polygon')
 
 hi.attr('points', '100,100 150,100 150,200 250,200 250,350 200,350 200,250 150,250 150,350 100,350')
+hi2.attr('points', '300,200 450,200 450,150 300,150')
+hi3.attr('points', '200,200 450,200 450,150 300,150')
 
 const circle = svg.append('circle');
 
@@ -11,12 +14,6 @@ svg.attr('height', window.innerHeight);
 
 circle.attr('r', 30);
 circle.attr('fill', 'red');
-
-hallo4.attr('x', 300);
-hallo4.attr('y', 200);
-hallo4.attr('height', 150);
-hallo4.attr('width', 50);
-
 
 svg.on('mousemove', (event) => {
     circle.attr('cx', event.x);
