2b002eb7082a92945481959482ca5336e4832173
Tristanchaang <tristan.chaang@gmail.com>
2024-07-30 03:23:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -21,14 +21,26 @@ function nearestMultiple(num, mul) {
     return Math.round(num/mul) * mul
 }
 
-function processInput() {
-    if (clickqueue && inputstatus) {
-        new node(clickqueue[0][0], clickqueue[0][1], inputstatus)
-    }
+let nodeisclicked = false;
+function nodeClicked(thisid) {
+    console.log("Node Selected: " + thisid)
+    const findhtml = d3.select("#"+thisid).select("circle");
+    clickqueue.push([Number(findhtml.attr("cx")),Number(findhtml.attr("cy"))])
+    nodeisclicked = true;
 }
 
-function nodeClicked() {
-    console.log("Node Selected")
+function midPoint(coord1,coord2) {
+    return [(Number(coord1[0]) + Number(coord2[0]))/2, 
+    (Number(coord1[1]) + Number(coord2[1]))/2];
+}
+
+function findNode(coord) {
+    for (let curnode of Object.keys(adjlist)) {
+        curnode = curnode.split(",")
+        if (curnode[0] == coord[0] && curnode[1] == coord[1]) {
+            return curnode;
+        }
+    }
 }
 
 const space = d3.select("body")
@@ -47,16 +59,19 @@ animate((elapsed) => {
     }, 2000)
 
 nodelist = Object()
+adjlist = Object()
 
 class node {
     constructor(x, y, label) {
         this.coord = [x,y];
         this.label = label;
         
+        adjlist[[x,y,label]] = [];
+
         nodelist[[x,y]] = svg.append("g")
         setattrs(nodelist[[x,y]], {
-            "id": "node:"+label,
-            "onclick": "nodeClicked()",
+            "id": "node"+label,
+            "onclick": "nodeClicked(this.id)",
             "style": "cursor: pointer;"
         })
 
@@ -81,18 +96,41 @@ class node {
     }
 }
 
-const N = 5;
+class edge {
+    constructor(node1, node2, label, arrow=true, weight=0, bend=0) {
+        this.start = node1;
+        this.end = node2;
+        this.label = label;
+        this.arrow = arrow;
+        this.weight = weight;
+        this.bend = bend;
 
-for (let i = 0; i < N; i++) {
-    new node(200 + 100 * Math.sin(2 * Math.PI * i / N), 
-    200 - 100 * Math.cos(2 * Math.PI * i / N), String(i))
+        const thisedge = svg.insert("path", "#divider");
+        
+        adjlist[node1].push([node2, thisedge]);
+
+        if (!arrow) {adjlist[node2].push([node1, thisedge])};
+
+        const midpoint = midPoint(node1.slice(0,2), node2.slice(0,2))
+
+        setattrs(thisedge, {
+            "style": "fill:none; stroke:black; stroke-width:5;",
+            "d": "M " + node1[0] + " " + node1[1] 
+            + " Q " + midpoint[0] + " " + midpoint[1] 
+            + " " + node2[0] + " " + node2[1] 
+            + "",
+        })
+    }
 }
 
 let clickqueue = [];
 let inputstatus = "";
 
 svg.on('click', (event) => {
-    clickqueue.push([nearestMultiple(event.x,50), nearestMultiple(event.y,50)])
+    if (!nodeisclicked) {
+        clickqueue.push([nearestMultiple(event.x,50), nearestMultiple(event.y,50)])
+    }
+    nodeisclicked = false;
     console.log(clickqueue)
 })
 
@@ -111,7 +149,21 @@ d3.select("body").on('keydown', (event) => {
     console.log(inputstatus)
 })
 
-svg.insert("path", "#divider")
-    .attr("d", "M 100 350 Q 250 50 400 350")
-    .attr("style", "fill:none; stroke:black; stroke-width:5;")
+function processInput() {
+    if (clickqueue.length === 1 && inputstatus) {
+        new node(clickqueue[0][0], clickqueue[0][1], inputstatus)
+    }
 
+    if (clickqueue.length === 2) {
+        const start = findNode(clickqueue[0]);
+        const end = findNode(clickqueue[1]);
+        new edge(start, end, inputstatus)
+    }
+}
+
+const N = 5;
+
+for (let i = 0; i < N; i++) {
+    let hi = new node(200 + 100 * Math.sin(2 * Math.PI * i / N), 
+    200 - 100 * Math.cos(2 * Math.PI * i / N), "a"+String(i))
+}
