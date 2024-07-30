47b18a82dd5f70c6299947671b18b19f84f08268
Tristanchaang <tristan.chaang@gmail.com>
2024-07-30 00:18:46 -0400

    china trip

--- main.js
+++ main.js
@@ -1,25 +1,76 @@
-const svg = d3.select('#app').append('svg');
-const hi = svg.append('polygon')
-const hi2 = svg.append('polygon')
-const hi3 = svg.append('polygon')
+function changeattr(shapelist,prop,val) {
+    for (let shape of shapelist) {
+        shape.attr(prop, val)
+    }
+}
 
-hi.attr('points', '100,100 150,100 150,200 250,200 250,350 200,350 200,250 150,250 150,350 100,350')
-hi2.attr('points', '300,200 450,200 450,150 300,150')
-hi3.attr('points', '200,200 450,200 450,150 300,150')
+function setattrs(shape, dict) {
+    for (let [prop, val] of Object.entries(dict)) {
+        shape.attr(prop, val)
+    }
+}
 
-const circle = svg.append('circle');
+function animate(func, duration, delay=0) {
+    let t = d3.timer((elapsed) => {
+        func(elapsed);
+        if (elapsed >= duration) {t.stop();}
+        }, delay)
+}
 
-svg.attr('width', window.innerWidth);
-svg.attr('height', window.innerHeight);
+const space = d3.select("#app")
+const svg = space.append("svg")
+setattrs(svg, {'width': window.innerWidth, 'height': window.innerHeight})
 
-circle.attr('r', 30);
-circle.attr('fill', 'red');
+const circle = svg.append("circle")
 
-svg.on('mousemove', (event) => {
-    circle.attr('cx', event.x);
-    circle.attr('cy', event.y);
-});
+setattrs(circle, {"r": 40, "fill": "red"})
 
-svg.on('click', (event) => {
-    console.log(event.x, event.y);
-});
+animate((elapsed) => {
+    setattrs(circle, {
+        "cx": 800+100*Math.cos(2*Math.PI*elapsed/1000), 
+        "cy": 200+100*Math.sin(2*Math.PI*elapsed/1000)})
+    }, 2000)
+
+nodelist = Object()
+
+class node {
+    constructor(x, y, label) {
+        this.coord = [x,y];
+        this.label = label;
+        
+        nodelist[[x,y]] = svg.append("g")
+        nodelist[[x,y]].attr("id", "node:"+label)
+
+        let circ = nodelist[[x,y]].append("circle")
+        setattrs(circ, {
+            "cx": x, "cy": y, "r": 25, 
+            "fill": "white", 
+            "stroke": "black",
+            "stroke-width": 5,
+            "fill-opacity": 0,
+        })
+
+        let labe = nodelist[[x,y]].append("text")
+        labe.text(String(label))
+        setattrs(labe, {
+            "x": x, "y": y,
+            "text-anchor": "middle",
+            "dominant-baseline": "central",
+            "font-size": 25,
+            // "font-family": "Arial, Helvetica, sans-serif",
+            "font-weight": "bold"
+        })
+        
+    }
+    
+}
+
+const N = 15;
+
+for (let i = 0; i < N; i++) {
+    new node(200 + 100 * Math.sin(2 * Math.PI * i / N), 
+    200 - 100 * Math.cos(2 * Math.PI * i / N), String(i))
+}
+
+
+console.log(10)
